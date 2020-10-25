d04428ca2efc257bd08fd28d03cc6be386b3c283
raul <raul.carrizo@inquisitive.com>
2020-10-26 09:24:13 +1100

    Update footer styles/layout

--- footer.php
+++ footer.php
@@ -5,33 +5,36 @@
 ?>
 	<footer class="footer">
 		<div class="footer-wrapper">
-			<div class="footer-logo-wrap">
-				<img src="<?php echo get_template_directory_uri(); ?>/static/images/logo-horizontal-white.svg" alt="" />
-			</div>
-			<div class="footer-description">
-				<p><?php echo get_option( 'blogdescription' ) ?></p>
+			<div>
+				<div class="footer-logo-wrap">
+					<img src="<?php echo get_template_directory_uri(); ?>/static/images/logo_stacked.svg" alt="" />
+				</div>
+				<div class="footer-description">
+					<p><?php echo get_option( 'blogdescription' ) ?></p>
+					<div class="footer-legals">
+						<span>&copy;2020 by COAST 4C Limited - ABN 92 641 553 82</span>
+					</div>
+				</div>
 			</div>
 			<div class="footer-content">
 				<div class="footer-block">
+					<h3 class="footer-block-title">Company</h3>
 					<?php
 						echo wp_nav_menu('footer-1');
 					?>
 				</div>
 				<div class="footer-block">
+					<h3 class="footer-block-title">Contact</h3>
 					<div class="footer-info">
 						<p>EMAIL</p>
 						<div>info@coast4c.com</div>
 					</div>
 					<div class="footer-info">
 						<p>POST</p>
-						<div>info@coast4c.com</div>
-					</div>
-					<div class="footer-info">
-						<div>ABN 92 641 553 82</div>
+						<div>Cronulla, NSW 2230, Australia</div>
 					</div>
 				</div>
 			</div>
-			<?php get_template_part('partials/footer', 'legals'); ?>
 		</div>
 	</footer>
 	<?php wp_footer(); ?>
