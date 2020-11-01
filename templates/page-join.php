9a5208469c86d66e739309238217ceb60195e5a7
raul <raul.carrizo@inquisitive.com>
2020-11-01 19:21:18 +1100

    Remove some team related csss

--- templates/page-join.php
+++ templates/page-join.php
@@ -8,19 +8,11 @@
 get_header();
 ?>
 
-<div class="row">
-  <div class="row-title">
-    <h1><?php echo get_the_title(); ?></h1>
-  </div>
-</div>
-
-<div class="row-join">
 <?php 
   if (have_posts()): while (have_posts()): the_post();
     the_content(); 
   endwhile; endif;
 ?>
-</div>
 
 <?php 
   get_footer(); 
