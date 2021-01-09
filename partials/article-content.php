866dec88cc90d7a52471c6334f730bc8df16c915
raul <raul.carrizo@inquisitive.com>
2021-01-09 11:09:10 +1100

    Blog layout updates

--- partials/article-content.php
+++ partials/article-content.php
@@ -33,41 +33,50 @@
 
 <?php if ( ! empty( $categories ) && $categories[0]->name != 'Research'): ?>
   <div class="blog-image-wrap">
-    <div class="blog-image" style="background-image: url(<?php echo $img_url[0] ?>);" data-type="background"></div>
+    <div class="grid blog-post-columns">
+      <div class="blog-image" style="background-image: url(<?php echo $img_url[0] ?>);" data-type="background"></div>
+      <div class="blog-post-side"></div>
+      <div class="blog-post-main">
+        <div class="blog-title-wrapper">
+          <h1 class="blog-title"><?php the_title(); ?></h1>
+          <div class="blog-date-wrapper">
+            <div class="blog-date">
+              <?php echo get_the_time( get_option('date_format')) ?>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
   </div>
 
   <div class="grid blog-post-columns">
     <div class="blog-post-side">
+      <div class="blog-author">
+        <div class="blog-author-avatar">
+          <?php echo get_avatar( get_the_author_meta( 'ID' ), 60 ); ?>
+        </div>
+        <div class="blog-author-description">
+          <strong><?php the_author_meta('display_name'); ?></strong>
+          <?php the_author_description(); ?>
+        </div>
+      </div>
+
       <div class="blog-category">
         <?php 
           $categories = get_the_category();
           if ( ! empty( $categories ) && $categories[0]->name != 'Uncategorised'):
         ?>
-          <div class="blog-pill blog-post-category">
-            <a href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ) ?>"><?php echo $categories[0]->name; ?></a>
-          </div>
+          <a class="blog-pill blog-post-category" href="<?php echo esc_url( get_category_link( $categories[0]->term_id ) ) ?>"><?php echo $categories[0]->name; ?></a>
         <?php endif; ?>
       </div>
-      <div class="blog-title">
-        <?php the_title(); ?>
-      </div>
-      <div class="blog-author">
-        <?php if (get_the_author_meta('user_login') != 'warhol'): ?>
-          Written by <strong><?php the_author_meta('display_name'); ?></strong>
-        <?php endif; ?>
-      </div>
-      <div class="blog-date">
-        <?php echo get_the_time( get_option('date_format')) ?>
-      </div>
+
+     
       <div class="blog-post-details">
         <?php get_template_part( 'partials/module', 'share' ); ?>
       </div>
     </div>
 
     <div class="blog-post-main">
-      <div class="blog-title">
-        <?php the_title(); ?>
-      </div>
       <div class="blog-content">
         <?php the_content(); ?>
       </div>
